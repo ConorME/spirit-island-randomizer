7850487516bb9c88c7733f45546a2fbcae232fed
ConorME <73617105+ConorME@users.noreply.github.com>
2024-08-25 14:36:50 -0400

    Update script.js with additional spirits.

--- script.js
+++ script.js
@@ -10,11 +10,35 @@ const spirits = [
   "Sharp Fangs Behind the Leaves",
   "Keeper of the Forbidden Wilds",
   "Heart of the Wildfire",
-  "Serpent Slumbering Beneath the Island"
+  "Serpent Slumbering Beneath the Island",
+  "Grinning Trickster Stirs Up Trouble",
+  "Lure of the Deep Wilderness",
+  "Many Minds Move as One",
+  "Shifting Memory of Ages",
+  "Stone's Unyielding Defiance",
+  "Volcano Looming High",
+  "Shroud of Silent Mist",
+  "Vengeance as a Burning Plague",
+  "Fractured Days Split the Sky",
+  "Starlight Seeks its Form",
+  "Downpour Drenches the World",
+  "Finder of Paths Unseen",
+  "Devouring Teeth Lurk Underfoot",
+  "Eyes Watch from the Trees",
+  "Famotherless Mud of the Swamp",
+  "Rising Heat of Stone and Sand",
+  "Sun-Bright Whirlwind",
+  "Ember-Eyed Behemoth",
+  "Hearth-Vigil",
+  "Towering Roots of the Jungle",
+  "Breath of Darkness Down Your Spine",
+  "Relentless Gaze of the Sun",
+  "Wandering Voice Keens Delirium",
+  "Wounded Waters Bleeding",
+  "Dances Up Earthquakes"
 ];
 
 document.getElementById('randomize-btn').addEventListener('click', function() {
   const randomSpirit = spirits[Math.floor(Math.random() * spirits.length)];
   document.getElementById('selected-spirit').textContent = randomSpirit;
 });
-
